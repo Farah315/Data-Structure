e3b52f185698d3588e9b21c7efec9be8a0360ce2
Farah <fara734isgreat@gmail.com>
2025-02-21 18:50:00 +0300

    first ds project

--- src/treasurebag/TreasureBag.java
+++ src/treasurebag/TreasureBag.java
@@ -1,15 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package treasurebag;
 
 import java.util.Scanner;
 
-/**
- *
- * @author Al-Qema
- */
 public class TreasureBag {
 
     public static void main(String[] args) {
